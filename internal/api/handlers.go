cbe4ade893dccfe561c6848d007b236c9bc2c39d
friday <hbarot@localhost.localdomain>
2025-06-28 16:31:00 +0200

    slight conditional statement

--- internal/api/handlers.go
+++ internal/api/handlers.go
@@ -11,6 +11,9 @@ func handleForecast(w http.ResponseWriter, r *http.Request) {
 	lat := r.URL.Query().Get("lat")
 	lon := r.URL.Query().Get("lon")
 	lang := r.URL.Query().Get("lang")
+	if lang == "" {
+		lang = "en"
+	}
 	if lat == "" || lon == "" {
 		http.Error(w, "Missing lat or lon", http.StatusBadRequest)
 		return
